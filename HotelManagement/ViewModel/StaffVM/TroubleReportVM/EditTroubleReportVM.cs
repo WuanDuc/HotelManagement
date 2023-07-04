bf726c5b187e568687fd1228d637fcaae7852327
Lê Quang Nhân <21522402@gm.uit.edu.vn>
2023-07-04 20:08:31 +0700

    Nhan add logic staff

---
+++ HotelManagement/ViewModel/StaffVM/TroubleReportVM/EditTroubleReportVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.ViewModel.StaffVM.TroubleReportVM
+{
+    internal class EditTroubleReportVM
+    {
+    }
+}
