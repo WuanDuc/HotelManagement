3a6f1774bd4717145139b7cc0e61fb2e3f140eb5
LAPTOP-S74F9L51\Ideapad gaming 3 <univerbachtuoc@gmail.com>
2023-11-10 23:34:17 +0700

    fix

---
+++ HotelManagementTests/ViewModel/AdminVM/CustomerManagementVM/CustomerManagementVMTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelManagement.ViewModel.AdminVM.CustomerManagementVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.ViewModel.AdminVM.CustomerManagementVM.Tests
+{
+    [TestClass()]
+    public class CustomerManagementVMTests
+    {
+        [TestMethod()]
+        public void AddNewCustomerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ TestHotelManagement/AddCustomerVMTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HotelManagement.DTOs;
+using HotelManagement.Model.Services;
+using HotelManagement.View.CustomMessageBoxWindow;
+using HotelManagement;
+using HotelManagement.ViewModel.AdminVM.CustomerManagementVM;
+using Moq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TestHotelManagement
+{
+    public class AddCustomerVMTest
+    {
+    }
+}
