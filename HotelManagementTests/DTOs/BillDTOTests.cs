dff8622496183c0fe84e49796fa2c05fbee6f76b
nyb1711 <nyb1711@gmail.com>
2024-01-04 06:58:31 +0700

    Add unit test

--- HotelManagementTests/DTOs/BillDTOTests.cs
+++ HotelManagementTests/DTOs/BillDTOTests.cs
@@ -1,5 +1,6 @@
 ﻿using HotelManagement.Model.Services;
 using HotelManagement.Model;
+using HotelManagement.DTOs;
 using HotelManagementTests.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -10,6 +11,8 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HotelManagement.Utilities;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace HotelManagement.DTOs.Tests
 {
@@ -18,5 +21,204 @@ namespace HotelManagement.DTOs.Tests
     /// </summary>
     [TestClass]
     public class BillDTOTests
-    { }
+    {
+        [TestMethod()]
+        public void RoomName_Correct()
+        {
+            BillDTO bill = new BillDTO()
+            {
+                RoomNumber = 101,
+            };
+
+            string expected = "Phòng " + bill.RoomNumber;
+            Assert.AreEqual(expected, bill.RoomName);
+        }
+
+        [TestMethod()] 
+        public void RoomPriceStr_Correct()
+        {
+            BillDTO bill = new BillDTO()
+            {
+                RoomPrice = 200,
+            };
+
+            string expected = Helper.FormatVNMoney((double)200);
+
+            Assert.AreEqual(expected, bill.RoomPriceStr);
+        }
+
+        [TestMethod()]
+        public void DayNumber_Correct()
+        {
+            BillDTO bill = new BillDTO()
+            {
+                CheckOutDate = new DateTime(2023, 12, 20),
+                StartDate = new DateTime(2023, 12,18),
+            };
+
+         
+            int expected = 2;
+
+            Assert.AreEqual(expected, bill.DayNumber);
+        }
+
+        [TestMethod()]
+        public void DayNumber_StartDateNull()
+        {
+            BillDTO bill = new BillDTO()
+            {
+                CheckOutDate = new DateTime(2023, 12, 20),
+                StartDate = null,
+            };
+
+            Assert.AreEqual(0, bill.DayNumber);
+        }
+
+        [TestMethod()]
+        public void DayNumber_StartDateNull_CheckoutDateNull()
+        {
+            BillDTO bill = new BillDTO()
+            {
+                CheckOutDate = null,
+                StartDate = null,
+            };
+
+            Assert.AreEqual(0, bill.DayNumber);
+        }
+
+        public void DayNumber_CheckoutDateNull()
+        {
+            BillDTO bill = new BillDTO()
+            {
+                CheckOutDate = null,
+                StartDate = new DateTime(2023, 12, 18),
+            };
+
+            Assert.AreEqual(0, bill.DayNumber);
+        }
+
+        [TestMethod()]
+        public void ServicePriceTemp_Correct()
+        {
+            double expected = 0;
+            BillDTO bill = new BillDTO();
+
+            Assert.AreEqual(expected, bill.ServicePriceTemp);
+        }
+
+        [TestMethod()]
+        public void ServicePriceTempStr_Correct()
+        {
+            double expected = 0;
+            BillDTO bill = new BillDTO();
+            
+
+            Assert.AreEqual(Helper.FormatVNMoney(expected), bill.ServicePriceTempStr);
+        }
+
+        [TestMethod()]
+        public void TroublePriceTemp_Correct()
+        {
+            List<TroubleByCustomerDTO> list = new List<TroubleByCustomerDTO>()
+            {
+                new TroubleByCustomerDTO()
+                {
+                    PredictedPrice = 220,
+                },
+                new TroubleByCustomerDTO()
+                {
+                    PredictedPrice = 420,
+                },
+            };
+
+            BillDTO bill = new BillDTO() {
+                ListTroubleByCustomer = list,
+            };
+
+            double t = 640;
+            Assert.AreEqual(t, bill.TroublePriceTemp);
+        }
+
+        [TestMethod()]
+        public void TroublePriceTempStr_Correct()
+        {
+            List<TroubleByCustomerDTO> list = new List<TroubleByCustomerDTO>()
+            {
+                new TroubleByCustomerDTO()
+                {
+                    PredictedPrice = 220,
+                },
+                new TroubleByCustomerDTO()
+                {
+                    PredictedPrice = 420,
+                },
+            };
+
+            BillDTO bill = new BillDTO()
+            {
+                ListTroubleByCustomer = list,
+            };
+
+
+            double t = 640;
+            Assert.AreEqual(Helper.FormatVNMoney(t), bill.TroublePriceTempStr);
+        }
+
+        [TestMethod()]
+
+        public void TotalPriceTemp_Correct()
+        {
+            List<TroubleByCustomerDTO> list = new List<TroubleByCustomerDTO>()
+            {
+                new TroubleByCustomerDTO()
+                {
+                    PredictedPrice = 220,
+                },
+                new TroubleByCustomerDTO()
+                {
+                    PredictedPrice = 420,
+                },
+            };
+
+            BillDTO bill = new BillDTO()
+            {
+                ListTroubleByCustomer = list,
+                CheckOutDate = new DateTime(2023, 12, 20),
+                StartDate = new DateTime(2023, 12, 18),
+                RoomPrice = 200,
+            };
+
+
+            double expected = 640 + 2 * (double)200;
+            Assert.AreEqual(expected, bill.TotalPriceTemp);
+        }
+
+        [TestMethod()]
+        public void TotalPriceTempStr_Correct()
+        {
+            List<TroubleByCustomerDTO> list = new List<TroubleByCustomerDTO>()
+            {
+                new TroubleByCustomerDTO()
+                {
+                    PredictedPrice = 220,
+                },
+                new TroubleByCustomerDTO()
+                {
+                    PredictedPrice = 420,
+                },
+            };
+
+            BillDTO bill = new BillDTO()
+            {
+                ListTroubleByCustomer = list,
+                CheckOutDate = new DateTime(2023, 12, 20),
+                StartDate = new DateTime(2023, 12, 18),
+                RoomPrice = 200,
+            };
+
+
+            double expected = 640 + 2 * (double)200;
+            Assert.AreEqual(Helper.FormatVNMoney(expected), bill.TotalPriceTempStr);
+        }
+    }
 }
---
+++ HotelManagementTests/Utils/ConstantTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HotelManagement.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HotelManagementTests.Utils
+{
+    [TestClass()]
+    public class ConstantTests
+    {
+        [TestMethod()]
+        public void ROOM_TYPE_A()
+        {
+            Assert.AreEqual("Phòng thường", ROOM_TYPE.ROOM_TYPE_A);
+        }
+
+        [TestMethod()]
+        public void ROOM_TYPE_B()
+        {
+            Assert.AreEqual("Phòng vip", ROOM_TYPE.ROOM_TYPE_B);
+        }
+
+        [TestMethod()]
+        public void ROOM_TYPE_C()
+        {
+            Assert.AreEqual("Phòng thương gia", ROOM_TYPE.ROOM_TYPE_C);
+        }
+
+        [TestMethod()]
+        public void ROOM_STATUS_R()
+        {
+            Assert.AreEqual("Phòng trống", ROOM_STATUS.READY);
+        }
+
+        [TestMethod()]
+        public void ROOM_STATUS_B()
+        {
+            Assert.AreEqual("Phòng đã đặt", ROOM_STATUS.BOOKED);
+        }
+
+        [TestMethod()]
+        public void ROOM_STATUS_Re()
+        {
+            Assert.AreEqual("Phòng đang thuê", ROOM_STATUS.RENTING);
+        }
+
+        [TestMethod()]
+        public void ROOM_CLEANING_STATUS_C()
+        {
+            Assert.AreEqual("Đã dọn dẹp", ROOM_CLEANING_STATUS.CLEANED);
+        }
+
+        [TestMethod()]
+        public void ROOM_CLEANING_STATUS_NC()
+        {
+            Assert.AreEqual("Chưa dọn dẹp", ROOM_CLEANING_STATUS.NOT_CLEANING_YET);
+        }
+
+        [TestMethod()]
+        public void ROOM_CLEANING_STATUS_R()
+        {
+            Assert.AreEqual("Sửa chữa", ROOM_CLEANING_STATUS.REPAIRING);
+        }
+
+        [TestMethod()]
+        public void HOTEL_INFO_Test()
+        {
+            Assert.AreEqual("0123456789", HOTEL_INFO.PHONE);
+        }
+    }
+}
